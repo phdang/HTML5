1b5cbbea521aab29fefd68ea74db2de3c5ee81a8
phdang <phdang94@yahoo..com>
2017-11-29 03:54:00 +0700

    complete responsive and toggled-menu-click in WorldOfDang

---
+++ worldofdang-js.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function($) {
+
+  var heightOfContainer = $('body').width();
+  console.log(heightOfContainer);
+
+  $('div img').mouseenter(function() {
+
+    heightOfContainer = $('body').width();
+    console.log(heightOfContainer);
+
+    imageZoomEffect(heightOfContainer);
+
+  });
+})
+
+
+function imageZoomEffect(heightOfContainer) {
+
+  jQuery(document).ready(function($) {
+
+    if (heightOfContainer > 768) {
+
+      $('.product img').addimagezoom({ // single image zoom
+        zoomrange: [3],
+
+        magnifiersize: [300, 300],
+        magnifierpos: 'right',
+        cursorshade: false,
+        largeimage: 'Koala.jpg' //<-- No comma after last option!
+      });
+
+      $('.fjords img').addimagezoom({ // single image zoom
+        zoomrange: [3],
+        magnifiersize: [300, 300],
+        magnifierpos: 'right',
+        cursorshade: false,
+        largeimage: 'fjords.jpg' //<-- No comma after last option!
+      });
+    } else {
+
+      $('.zoomtracker').remove();
+
+    }
+
+  });
+}
